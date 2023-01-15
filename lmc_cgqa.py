7f365f90a4f4bd64ca07b8865b1ff7d7a05cfaa2
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2023-01-15 13:47:16 +0800

    offline mode init wandb

--- lmc_cgqa.py
+++ lmc_cgqa.py
@@ -1177,7 +1177,7 @@ if __name__== "__main__":
         if args_generator.debug:
             pr_name='test'
         # if not args_generator.debug:
-        run = wandb.init(project=pr_name, notes=args_generator.wand_notes, settings=wandb.Settings(start_method="fork"), reinit=(args_generator.n_runs>1))
+        run = wandb.init(mode='offline', project=pr_name, notes=args_generator.wand_notes, settings=wandb.Settings(start_method="fork"), reinit=(args_generator.n_runs>1))
         if not args_generator.debug:
             wandb.config.update(args_generator, allow_val_change=False)
         set_seed(manualSeed=args_generator.seed)
