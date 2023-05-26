750ca74ec615523688f0c362ec4641409f482432
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2023-05-26 10:33:54 +0800

    debug

--- Bash/mntdp_cobj-vit-lr1e-4.sh
+++ Bash/mntdp_cobj-vit-lr1e-4.sh
@@ -1,3 +1,3 @@
 
 export WANDB_MODE=offline
-CUDA_VISIBLE_DEVICES=0 python3 mntdp_cgqa.py --exp_name mntdp_vit_cobj_3tasks_lr1e-4 --lr 1e-4 --n_tasks 5 --dataset cobj --seed 1234 --module_type vit_block --hidden_size=384 --batch_size=100 --depth 5 --redo_final_test 1 --epochs 200 --image_size 224 --momentum_bn 0.1 --pr_name mntdp_vit_cobj --copy_batchstats 1 --track_running_stats_bn 1 --task_sequence s_minus --gating MNTDP --shuffle_test 0 --wdecay 0
+CUDA_VISIBLE_DEVICES=0 python3 mntdp_cgqa.py --exp_name mntdp_vit_cobj_3tasks-_lr1e-4 --lr 1e-4 --n_tasks 3 --dataset cobj --seed 1234 --module_type vit_block --hidden_size=384 --batch_size=100 --depth 5 --redo_final_test 1 --epochs 200 --image_size 224 --momentum_bn 0.1 --pr_name mntdp_vit_cobj --copy_batchstats 1 --track_running_stats_bn 1 --task_sequence s_minus --gating MNTDP --shuffle_test 0 --wdecay 0
